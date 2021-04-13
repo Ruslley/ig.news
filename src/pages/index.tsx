fcd2e70c1fdcb186e34db3e60eaa2f7f4339538a
Ruslley <r.sales07@gmail.com>
2021-04-13 09:02:02 -0300

    app

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.title}>
+        Hello world, Next!
+      </h1>
+    </div>
+  )
+}
